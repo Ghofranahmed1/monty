97ad92743041ff417e0c074649c868859d9231cc
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-12-14 16:19:12 -0800

    swap opcode added

---
+++ swap.c
@@ -0,0 +1,32 @@
+#include "head.h"
+/**
+ * _swap - swaps the top two elements of the stack
+ *
+ * @head: head of the linked list
+ * @lnum: line number
+ * Return: no return
+ */
+void _swap(stack_t **head, unsigned int lnum)
+{
+        int b = 0;
+        stack_t *aux = NULL;
+
+        aux = *head;
+
+        for (; aux != NULL; aux = aux->next, b++)
+                ;
+
+        if (b < 2)
+        {
+                dprintf(2, "L%u: can't swap, stack too short\n", lnum);
+                free_gv();
+                exit(EXIT_FAILURE);
+        }
+
+        aux = *head;
+        *head = (*head)->next;
+        aux->next = (*head)->next;
+        aux->prev = *head;
+        (*head)->next = aux;
+        (*head)->prev = NULL;
+}
