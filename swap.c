468c07985b320840c680287469a2ea384fdab5eb
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-12-14 18:20:43 -0800

    update

--- swap.c
+++ swap.c
@@ -10,25 +10,25 @@
  */
 void _swap(stack_t **head, unsigned int lnum)
 {
-        int b = 0;
-        stack_t *aux = NULL;
+	int b = 0;
+	stack_t *aux = NULL;
 
-        aux = *head;
+	aux = *head;
 
-        for (; aux != NULL; aux = aux->next, b++)
-                ;
+	for (; aux != NULL; aux = aux->next, b++)
+		;
 
-        if (b < 2)
-        {
-                dprintf(2, "L%u: can't swap, stack too short\n", lnum);
-                free_gv();
-                exit(EXIT_FAILURE);
-        }
+	if (b < 2)
+	{
+		dprintf(2, "L%u: can't swap, stack too short\n", lnum);
+		free_gv();
+		exit(EXIT_FAILURE);
+	}
 
-        aux = *head;
-        *head = (*head)->next;
-        aux->next = (*head)->next;
-        aux->prev = *head;
-        (*head)->next = aux;
-        (*head)->prev = NULL;
+	aux = *head;
+	*head = (*head)->next;
+	aux->next = (*head)->next;
+	aux->prev = *head;
+	(*head)->next = aux;
+	(*head)->prev = NULL;
 }
